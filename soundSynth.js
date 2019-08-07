65b09c66238e13094f6c21fd4bdf8f7305703ada
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-06 22:55:14 -0400

    Update soundSynth.js

--- soundSynth.js
+++ soundSynth.js
@@ -6,15 +6,14 @@ DESIGNED AND CREATED BY MICHAEL GARNER
 MONTREAL 2019
 */ 
 
+//SET UP CANVAS
 //canvas can now be called by this variable
 var canvas = document.querySelector('canvas');
-
 //set canvas height and width
 canvas.width = window.innerWidth - 10;
 canvas.height = window.innerHeight - 10;
 var windowWidth = window.innerWidth - 10;
 var windowHeight = window.innerHeight - 10;
-
 //Gives canvas API 2d drawing functions
 var c = canvas.getContext('2d');
 
